11ef156ee2a8feb5cd27a98d63d7987ddec223f1
Deepanshu Sinha <107sinha@gmail.com>
2022-07-12 00:22:36 +0530

    added problem statement

--- question_1.java
+++ question_1.java
@@ -1,3 +1,8 @@
+
+/**
+ * PayMoney. processes thousands of transactions daily amounting to crores of Rupees. They also have a daily target that they must achieve. Given a list of transactions done by PayMoney and a daily target, your task is to determine at which transaction PayMoney achieves the same. If the target is not achievable, then display the target is not achieved.
+ */
+
 import java.util.Scanner;
 
 class PayMoney {
